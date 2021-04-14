2989c406b8fae63d1dce06c95e1146fba9ee959c
Yazeed AlKhalaf <yazeedfady@gmail.com>
2021-04-14 15:56:17 +0300

    ➕ Add some UI for home view

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:steps_tracker/app/utils/colors.dart';
 
 import './home_view_model.dart';
 
@@ -14,9 +15,54 @@ class HomeView extends StatelessWidget {
         Widget child,
       ) {
         return Scaffold(
-          body: Center(
-            child: Text(
-              'HomeView',
+          body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Welcome ${model.currentUser?.firstName ?? ""},",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "Steps: ${model.currentUser?.stepsCount ?? ""}",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    await model.signOut();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: KColors.orange,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  icon: Icon(
+                    Icons.exit_to_app_rounded,
+                  ),
+                  label: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Text(
+                      "Sign Out",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         );
